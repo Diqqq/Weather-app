9ee900d9272e2e89ab457df5d19527fb8762d71b
Damian <diqqqtv@gmail.comgit config --global user.email damian.mariusz.pyszka@gmail.comgit config --global user.name Damiangit commit -m created>
2022-03-30 23:13:21 +0200

    added new SCSS and HTML structure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+const img1 = document.querySelector('.item1')
+const btn = document.querySelector('.arrow')
+const arrow = document.querySelector('.fas')
 
+
+const showImg = () => {
+    img1.classList.toggle('hide')
+
+	if (img1.classList.contains('hide')) {
+		arrow.style.transform = 'rotate(180deg)'
+	} else {
+		arrow.style.transform = 'rotate(0)'
+	}
+}
+
+btn.addEventListener('click', showImg)
