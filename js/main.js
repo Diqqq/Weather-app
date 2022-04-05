24b75dfac69cfb952f1f1a25fcb716a1b986bf27
Damian <diqqqtv@gmail.comgit config --global user.email damian.mariusz.pyszka@gmail.comgit config --global user.name Damiangit commit -m created>
2022-04-05 22:07:20 +0200

    Created ToDo List Project w/ JS

--- js/main.js
+++ js/main.js
@@ -1,53 +1,126 @@
-const converter = document.querySelector('#converter')
-const result = document.querySelector('.result')
-const convBtn = document.querySelector('.conv')
-const resetBtn = document.querySelector('.reset')
-const changeBtn = document.querySelector('.change')
-const one = document.querySelector('.one')
-const two = document.querySelector('.two')
+let todoInput //miejsce, gdzie użytkownik wpisuje treść zadania
+let errorInfo // info o braku zadań / konieczności wpisania tekstu
+let addBtn // przycisk add - dodaje nowe elementy do listy
+let ulList // lista zadań, tagi UL
+let newTodos// nowo dodany LI, nowe zadanie
 
-const swap = () => {
-	if (one.textContent === '°C') {
-		one.textContent = '°F'
-		two.textContent = '°C'
+let popup // popup
+let popupInfo // tekst w popupie, jak się doda pusty tekst
+let todoToEdit // edytowany Todo
+let popupInput // input w popupie
+let popupAddBtn // przycisk "zatwierdź" w popupie
+let popupCloseBtn // przycisk "anuluj" w popupie
+
+const main = () => {
+	//wywołuje nasze funkcje prepareDOM
+	prepareDOMElements()
+	prepareDOMEvents()
+}
+
+const prepareDOMElements = () => {
+	// pobieramy wszystkie elementy
+	todoInput = document.querySelector('.todo-input')
+	errorInfo = document.querySelector('.error-info')
+	addBtn = document.querySelector('.btn-add')
+	ulList = document.querySelector('.todolist ul')
+	popup = document.querySelector('.popup')
+	popupInfo = document.querySelector('.popup-info')
+	popupInput = document.querySelector('.popup-input')
+	popupAddBtn = document.querySelector('.accept')
+	popupCloseBtn = document.querySelector('.cancel')
+}
+
+const prepareDOMEvents = () => {
+	// nadajemy nasłuchiwanie
+	addBtn.addEventListener('click', addNewTodo)
+	ulList.addEventListener('click', checkClick)
+	popupCloseBtn.addEventListener('click', closePopup)
+	popupAddBtn.addEventListener('click', changeToDoText)
+	todoInput.addEventListener('keyup', enterKeyCheck)
+}
+
+const addNewTodo = () => {
+	if (todoInput.value !== '') {
+		newTodos = document.createElement('li')
+		newTodos.textContent = todoInput.value
+        createToolsArea()
+		ulList.append(newTodos)
+
+		todoInput.value = ''
+		errorInfo.textContent = ''
 	} else {
-		one.textContent = '°C'
-		two.textContent = '°F'
+		errorInfo.textContent = 'Wpisz treść zadania!'
 	}
-    result.textContent = ''
 }
 
-const celToFahr = () => {
-    const fahrenheit = converter.value * 1.8 + 32
-    result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`
-    converter.value = ''
+const createToolsArea = () => {
+	const toolsPanel = document.createElement('div')
+	toolsPanel.classList.add('tools')
+    newTodos.append(toolsPanel)
+
+	const btnComplete = document.createElement('button')
+	btnComplete.classList.add('complete')
+	btnComplete.innerHTML = '<i class="fas fa-check"></i>'
+
+	const btnEdit = document.createElement('button')
+	btnEdit.classList.add('edit')
+	btnEdit.textContent = 'EDIT'
+
+	const btnDelete = document.createElement('button')
+	btnDelete.classList.add('delete')
+	btnDelete.innerHTML = '<i class="fas fa-times">'
+
+    toolsPanel.append(btnComplete, btnEdit, btnDelete)
 }
 
-const fahrtoCel = () => {
-	const celsius = (converter.value - 32) / 1.8
-	result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`
-    converter.value = ''
+const checkClick = e => {
+	if (e.target.matches('.complete')) {
+		e.target.closest('li').classList.toggle('completed');
+		e.target.classList.toggle('completed')
+	} else if (e.target.matches('.edit')) {
+		editTodo(e)
+	} else if (e.target.matches('.delete')) {
+		deleteTodo(e)
+	}
 }
 
-const convert = () => {
-    if (converter.value !== '') {
-
-        if (one.textContent === '°C') {
-            celToFahr()
-        }  else {
-            fahrtoCel()
-        }
-
-    } else {
-        result.textContent = 'Musisz podać jakąś wartość'
-    }
+const editTodo = (e) => {
+	todoToEdit = e.target.closest('li')
+	popupInput.value = todoToEdit.firstChild.textContent
+	popup.style.display = 'flex'
 }
 
-const reset = () => {
-    result.textContent = ''
-    converter.value = ''
+const closePopup = () => {
+	popup.style.display = ''
+	popupInfo.textContent = ''
 }
 
-changeBtn.addEventListener('click', swap)
-convBtn.addEventListener('click', convert)
-resetBtn.addEventListener('click', reset)
+const changeToDoText = () => {
+	if(popupInput.value !== '') {
+		todoToEdit.firstChild.textContent = popupInput.value
+		popup.style.display = ''
+		popupInfo.textContent = ''
+	} else {
+		popupInfo.textContent = 'Musisz podać jakąś treść!'
+	}
+}
+
+const deleteTodo = (e) => {
+	e.target.closest('li').remove()
+	const allTodos = ulList.querySelectorAll('li')
+	
+	if(allTodos.length === 0) {
+		errorInfo.textContent = 'Brak zadań na liście'
+	}
+}
+
+const enterKeyCheck = e => {
+	if(e.key === 'Enter') {
+		addNewTodo()
+	}
+	
+}
+
+
+
+document.addEventListener('DOMContentLoaded', main)
